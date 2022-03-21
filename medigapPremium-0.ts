7a1a0bb384a1d32d1a6fc0cfc2105700c9fc2b18
juandps97 <82608797+juandps97@users.noreply.github.com>
2022-03-21 12:55:52 -0400

    Initial version of Medigap file

---
+++ medigapPremium-0.ts
@@ -0,0 +1,619 @@
+/* tslint:disable: object-literal-sort-keys */
+const premiums: any = {
+  generatedOn: '01/13/2020',
+  ak: {
+    zip: '99502',
+    fips: '02020',
+    planG: {
+      low: 102,
+      high: 197,
+      estimatedAnnualCost: 7630
+    },
+    partDMonthly: [
+      52.459122681549026
+    ]
+  },
+  al: {
+    zip: '35005',
+    fips: '01073',
+    planG: {
+      low: 99,
+      high: 165,
+      estimatedAnnualCost: 7700
+    },
+    partDMonthly: [
+      55.567851851852126
+    ]
+  },
+  ar: {
+    zip: '71612',
+    fips: '05069',
+    planG: {
+      low: 112,
+      high: 309,
+      estimatedAnnualCost: 8000
+    },
+    partDMonthly: [
+      47.933241082410774
+    ]
+  },
+  az: {
+    zip: '85002',
+    fips: '04013',
+    planG: {
+      low: 102,
+      high: 294,
+      estimatedAnnualCost: 7740
+    },
+    partDMonthly: [
+      59.5283536585366
+    ]
+  },
+  ca: {
+    zip: '90002',
+    fips: '06037',
+    planG: {
+      low: 110,
+      high: 253,
+      estimatedAnnualCost: 7870
+    },
+    partDMonthly: [
+      53.933892815076604
+    ]
+  },
+  co: {
+    zip: '80002',
+    fips: '08001',
+    planG: {
+      low: 113,
+      high: 200,
+      estimatedAnnualCost: 7850
+    },
+    partDMonthly: [
+      52.8860039564787
+    ]
+  },
+  ct: {
+    zip: '06002',
+    fips: '09003',
+    planG: {
+      low: 199,
+      high: 473,
+      estimatedAnnualCost: 8830
+    },
+    partDMonthly: [
+      58.69444444444439
+    ]
+  },
+  dc: {
+    zip: '20002',
+    fips: '11001',
+    planG: {
+      low: 82,
+      high: 176,
+      estimatedAnnualCost: 0
+    },
+    partDMonthly: [
+      52.137499999999996
+    ]
+  },
+  de: {
+    zip: '19702',
+    fips: '10003',
+    planG: {
+      low: 113,
+      high: 210,
+      estimatedAnnualCost: 7750
+    },
+    partDMonthly: [
+      55.0159090909091
+    ]
+  },
+  fl: {
+    zip: '32004',
+    fips: '12109',
+    planG: {
+      low: 203,
+      high: 472,
+      estimatedAnnualCost: 8620
+    },
+    partDMonthly: [
+      59.81245847176078
+    ]
+  },
+  ga: {
+    zip: '30003',
+    fips: '13135',
+    planG: {
+      low: 119,
+      high: 440,
+      estimatedAnnualCost: 7860
+    },
+    partDMonthly: [
+      54.274548544720794
+    ]
+  },
+  hi: {
+    zip: '96703',
+    fips: '15007',
+    planG: {
+      low: 39,
+      high: 128,
+      estimatedAnnualCost: 7750
+    },
+    partDMonthly: [
+      68.42202380952381
+    ]
+  },
+  ia: {
+    zip: '50002',
+    fips: '19001',
+    planG: {
+      low: 94,
+      high: 195,
+      estimatedAnnualCost: 7670
+    },
+    partDMonthly: [
+      50.641149068322974
+    ]
+  },
+  id: {
+    zip: '83202',
+    fips: '16005',
+    planG: {
+      low: 119,
+      high: 246,
+      estimatedAnnualCost: 8220
+    },
+    partDMonthly: [
+      46.56276041666661
+    ]
+  },
+  il: {
+    zip: '60004',
+    fips: '17031',
+    planG: {
+      low: 107,
+      high: 218,
+      estimatedAnnualCost: 7750
+    },
+    partDMonthly: [
+      47.93194301407508
+    ]
+  },
+  in: {
+    zip: '46012',
+    fips: '18035',
+    planG: {
+      low: 111.52941176470588,
+      high: 269.2549019607843,
+      estimatedAnnualCost: 7760
+    },
+    partDMonthly: [
+      46.746303142328884
+    ]
+  },
+  ks: {
+    zip: '66010',
+    fips: '20107',
+    planG: {
+      low: 108,
+      high: 222,
+      estimatedAnnualCost: 7920
+    },
+    partDMonthly: [
+      55.75015432098766
+    ]
+  },
+  ky: {
+    zip: '40006',
+    fips: '21041',
+    planG: {
+      low: 100,
+      high: 183,
+      estimatedAnnualCost: 7790
+    },
+    partDMonthly: [
+      36.71149080735412
+    ]
+  },
+  la: {
+    zip: '70002',
+    fips: '22051',
+    planG: {
+      low: 105,
+      high: 246,
+      estimatedAnnualCost: 7790
+    },
+    partDMonthly: [
+      64.4962672372799
+    ]
+  },
+  ma: {
+    zip: '01096',
+    fips: '25015',
+    planG: {
+      low: 94,
+      high: 171,
+      estimatedAnnualCost: 0
+    },
+    partDMonthly: [
+      52.69166666666662
+    ]
+  },
+  md: {
+    zip: '20603',
+    fips: '24017',
+    planG: {
+      low: 126,
+      high: 310,
+      estimatedAnnualCost: 7940
+    },
+    partDMonthly: [
+      54.895402298850605
+    ]
+  },
+  me: {
+    zip: '03902',
+    fips: '23031',
+    planG: {
+      low: 172,
+      high: 295,
+      estimatedAnnualCost: 8590
+    },
+    partDMonthly: [
+      40.950720164609045
+    ]
+  },
+  mi: {
+    zip: '48002',
+    fips: '26147',
+    planG: {
+      low: 116,
+      high: 376,
+      estimatedAnnualCost: 7780
+    },
+    partDMonthly: [
+      41.970713565304976
+    ]
+  },
+  mn: {
+    zip: '55018',
+    fips: '27037',
+    planG: {
+      low: 121,
+      high: 269,
+      estimatedAnnualCost: 0
+    },
+    partDMonthly: [
+      56.71372332597475
+    ]
+  },
+  mo: {
+    zip: '63010',
+    fips: '29099',
+    planG: {
+      low: 123,
+      high: 248,
+      estimatedAnnualCost: 8090
+    },
+    partDMonthly: [
+      50.64905020352785
+    ]
+  },
+  ms: {
+    zip: '38602',
+    fips: '28137',
+    planG: {
+      low: 108,
+      high: 245,
+      estimatedAnnualCost: 7720
+    },
+    partDMonthly: [
+      51.85177151120752
+    ]
+  },
+  mt: {
+    zip: '59003',
+    fips: '30075',
+    planG: {
+      low: 96,
+      high: 217,
+      estimatedAnnualCost: 7790
+    },
+    partDMonthly: [
+      64.23719135802463
+    ]
+  },
+  nc: {
+    zip: '27007',
+    fips: '37171',
+    planG: {
+      low: 94,
+      high: 209,
+      estimatedAnnualCost: 7680
+    },
+    partDMonthly: [
+      47.446100628930914
+    ]
+  },
+  nd: {
+    zip: '58002',
+    fips: '38017',
+    planG: {
+      low: 99,
+      high: 244,
+      estimatedAnnualCost: 7910
+    },
+    partDMonthly: [
+      79.72405660377368
+    ]
+  },
+  ne: {
+    zip: '68002',
+    fips: '31177',
+    planG: {
+      low: 94,
+      high: 219,
+      estimatedAnnualCost: 7620
+    },
+    partDMonthly: [
+      41.01858304297331
+    ]
+  },
+  nh: {
+    zip: '03032',
+    fips: '33015',
+    planG: {
+      low: 145,
+      high: 311,
+      estimatedAnnualCost: 8180
+    },
+    partDMonthly: [
+      49.59047619047623
+    ]
+  },
+  nj: {
+    zip: '07002',
+    fips: '34017',
+    planG: {
+      low: 139,
+      high: 269,
+      estimatedAnnualCost: 8050
+    },
+    partDMonthly: [
+      56.49989281886384
+    ]
+  },
+  nm: {
+    zip: '87002',
+    fips: '35061',
+    planG: {
+      low: 91,
+      high: 278,
+      estimatedAnnualCost: 7650
+    },
+    partDMonthly: [
+      57.80182072829128
+    ]
+  },
+  nv: {
+    zip: '89701',
+    fips: '32510',
+    planG: {
+      low: 102,
+      high: 205,
+      estimatedAnnualCost: 7790
+    },
+    partDMonthly: [
+      49.67795698924731
+    ]
+  },
+  ny: {
+    zip: '00501',
+    fips: '36103',
+    planG: {
+      low: 220,
+      high: 455,
+      estimatedAnnualCost: 9120
+    },
+    partDMonthly: [
+      53.977370689655515
+    ]
+  },
+  oh: {
+    zip: '43002',
+    fips: '39049',
+    planG: {
+      low: 97,
+      high: 169,
+      estimatedAnnualCost: 7700
+    },
+    partDMonthly: [
+      48.40568376068395
+    ]
+  },
+  ok: {
+    zip: '73002',
+    fips: '40051',
+    planG: {
+      low: 72,
+      high: 192,
+      estimatedAnnualCost: 7760
+    },
+    partDMonthly: [
+      38.781720430107534
+    ]
+  },
+  or: {
+    zip: '97002',
+    fips: '41005',
+    planG: {
+      low: 92,
+      high: 157,
+      estimatedAnnualCost: 7830
+    },
+    partDMonthly: [
+      56.35715604801483
+    ]
+  },
+  pa: {
+    zip: '15003',
+    fips: '42003',
+    planG: {
+      low: 83,
+      high: 294,
+      estimatedAnnualCost: 7940
+    },
+    partDMonthly: [
+      53.263367123832204
+    ]
+  },
+  ri: {
+    zip: '02802',
+    fips: '44007',
+    planG: {
+      low: 110,
+      high: 197,
+      estimatedAnnualCost: 7740
+    },
+    partDMonthly: [
+      35.91734693877551
+    ]
+  },
+  sc: {
+    zip: '29003',
+    fips: '45009',
+    planG: {
+      low: 90,
+      high: 386,
+      estimatedAnnualCost: 7580
+    },
+    partDMonthly: [
+      43.38561306223045
+    ]
+  },
+  sd: {
+    zip: '57004',
+    fips: '46027',
+    planG: {
+      low: 104,
+      high: 183,
+      estimatedAnnualCost: 7750
+    },
+    partDMonthly: [
+      75.17412698412693
+    ]
+  },
+  tn: {
+    zip: '37012',
+    fips: '47041',
+    planG: {
+      low: 85,
+      high: 249,
+      estimatedAnnualCost: 7640
+    },
+    partDMonthly: [
+      51.600947281713346
+    ]
+  },
+  tx: {
+    zip: '73344',
+    fips: '48453',
+    planG: {
+      low: 82,
+      high: 295,
+      estimatedAnnualCost: 7820
+    },
+    partDMonthly: [
+      53.20509145509139
+    ]
+  },
+  ut: {
+    zip: '84002',
+    fips: '49013',
+    planG: {
+      low: 92,
+      high: 207,
+      estimatedAnnualCost: 7660
+    },
+    partDMonthly: [
+      53.637603305785134
+    ]
+  },
+  va: {
+    zip: '20102',
+    fips: '51107',
+    planG: {
+      low: 101,
+      high: 215,
+      estimatedAnnualCost: 7640
+    },
+    partDMonthly: [
+      48.65696517412948
+    ]
+  },
+  vt: {
+    zip: '05009',
+    fips: '50027',
+    planG: {
+      low: 176,
+      high: 224,
+      estimatedAnnualCost: 8490
+    },
+    partDMonthly: [
+      44.22574525745259
+    ]
+  },
+  wa: {
+    zip: '98002',
+    fips: '53033',
+    planG: {
+      low: 171,
+      high: 273,
+      estimatedAnnualCost: 8570
+    },
+    partDMonthly: [
+      52.90623809523805
+    ]
+  },
+  wi: {
+    zip: '53020',
+    fips: '55015',
+    planG: {
+      low: 93,
+      high: 228,
+      estimatedAnnualCost: 0
+    },
+    partDMonthly: [
+      63.32632288506882
+    ]
+  },
+  wv: {
+    zip: '25301',
+    fips: '54039',
+    planG: {
+      low: 111,
+      high: 238,
+      estimatedAnnualCost: 7650
+    },
+    partDMonthly: [
+      46.3126806178375
+    ]
+  },
+  wy: {
+    zip: '82002',
+    fips: '56021',
+    planG: {
+      low: 100,
+      high: 198,
+      estimatedAnnualCost: 7640
+    },
+    partDMonthly: [
+      48.38703703703703
+    ]
+  }
+};
+
+export default premiums;
+/* tslint:enable: object-literal-sort-keys */
